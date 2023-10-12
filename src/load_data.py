f9c8d688d12e06376699da0d02ff8f634e118f90
Rohan <rohan.calculus@gmail.com>
2023-10-12 22:34:53 +0530

    Stage 1 - load_data complete

--- src/load_data.py
+++ src/load_data.py
@@ -1,1 +1,22 @@
- 
+# Read the data from data source
+# Save it in the data/raw for further process
+# Import Libraries 
+import os
+import argparse
+from get_data import read_params, get_data
+
+# Read the data from source and save it in the data/raw for further process
+def load_and_save(config_path):
+    config = read_params(config_path)
+    df = get_data(config_path)
+    new_cols = [col.replace(" ", "_") for col in df.columns]
+    raw_data_path = config["load_data"]["raw_dataset_csv"]
+    df.to_csv(raw_data_path, sep=",", index=False, header=new_cols)
+
+# Initialize command-line argument parsing
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    load_and_save(config_path=parsed_args.config)
+
