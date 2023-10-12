de8b3bfe8b0fe8923f0c030ea9bb9c76d8d10db9
Rohan <rohan.calculus@gmail.com>
2023-10-12 22:58:35 +0530

    Stage 2 - split__data complete

--- src/split_data.py
+++ src/split_data.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from get_data import read_params
 
+# Split the data into train and test dataframes and save them
 def split_and_save_data(config_path):
     config = read_params(config_path)
     test_data_path = config["split_data"]["test_path"]
